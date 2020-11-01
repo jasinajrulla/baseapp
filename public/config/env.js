c63bc133e9c7216857b408a90a3c062c85d71410
jasinajrulla <ajrulla.jasin@gmail.com>
2020-11-01 22:08:11 +0100

    Update env.js

--- public/config/env.js
+++ public/config/env.js
@@ -1,10 +1,10 @@
 window.env = {
     api: {
-        authUrl: 'http://localhost:9002/api/v2/barong',
-        tradeUrl: 'http://localhost:9002/api/v2/peatio',
-        applogicUrl: 'http://localhost:9002/api/v2/applogic',
-        rangerUrl: 'ws://localhost:9003/api/v2/ranger',
-        finexUrl: 'http://localhost:9002/api/v2/finex',
+        authUrl: 'https://trade.novadefi.com/api/v2/barong',
+        tradeUrl: 'https://trade.novadefi.com/api/v2/peatio',
+        applogicUrl: 'https://trade.novadefi.com/api/v2/applogic',
+        rangerUrl: 'ws://trade.novadefi.com/api/v2/ranger',
+        finexUrl: 'https://trade.novadefi.com/api/v2/finex',
     },
     minutesUntilAutoLogout: '5',
     withCredentials: false,
